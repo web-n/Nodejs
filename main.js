4c2bd8b24fb7823147c01684345fcf838353ad96
egoing <egoing@gmail.com>
2018-05-29 02:26:12 +0900

    5.웹서버로서 Nodejs

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
     response.writeHead(200);
-    console.log(__dirname + request.url);
-    response.end(fs.readFileSync(__dirname + request.url));
+    response.end(fs.readFileSync(__dirname + url));
+
 });
 app.listen(3000);
