ec576139e7c406eb781d6a3e6c3d1e7ffec61a1a
egoing <egoing@gmail.com>
2018-05-29 01:33:58 +0900

    7.2.변수-활용

---
+++ syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'k8805';
+var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. '+name;
+console.log(letter);
