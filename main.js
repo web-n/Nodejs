ba0647bd6eb7028074dcdf3274b28365a90f38d1
egoing <egoing@gmail.com>
2018-05-28 23:00:47 +0900

    delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function(request,response){
-    response.writeHead(200);
-    console.log(__dirname + request.url);
-    response.end("Hello egoing");
-});
-app.listen(3000);
