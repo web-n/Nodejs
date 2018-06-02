bd243ab3c5cbce3d0ccdf21cf1e81db9048f9094
egoing <egoing@gmail.com>
2018-06-02 13:43:39 +0900

    25.3.JavaScript - 함수의 출력

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,7 +2,7 @@ console.log(Math.round(1.6)); //2
 console.log(Math.round(1.4)); //1
 
 function sum(first, second){ // parameter
-  console.log(first+second);
+  return first+second;
 }
 
-sum(2,4); // argument
+console.log(sum(2,4)); // argument
