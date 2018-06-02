b351494a4a63047b0ac717afdf7fb910d58c29db
egoing <egoing@gmail.com>
2018-06-02 12:09:32 +0900

    25.1.JavaScript - 함수의 생성

---
+++ syntax/function.js
@@ -0,0 +1,23 @@
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+console.log('F');
+console.log('C');
+console.log('P');
+console.log('J');
+f123();
+console.log('U');
+console.log('A');
+console.log('Z');
+console.log('J');
+console.log('I');
+f123();
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
