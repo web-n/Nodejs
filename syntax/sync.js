da16baeebe35d419a86410a51f6783f9aac8565e
egoing <egoing@gmail.com>
2018-06-03 14:51:18 +0900

    28.2.동기와비동기2

---
+++ syntax/sync.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+/*
+//readFileSync
+console.log('A');
+var result = fs.readFileSync('syntax/sample.txt', 'utf8');
+console.log(result);
+console.log('C');
+*/
+
+
+console.log('A');
+fs.readFile('syntax/sample.txt', 'utf8', function(err, result){
+    console.log(result);
+});
+console.log('C');
