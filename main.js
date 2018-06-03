ecbb65278ebe726463ab829a7e8ccaffa787e70f
egoing <egoing@gmail.com>
2018-06-04 01:25:50 +0900

    36.웹앱-글수정 - 파일명 변경, 내용저장

--- main.js
+++ main.js
@@ -118,6 +118,23 @@ var app = http.createServer(function(request,response){
           response.end(template);
         });
       });
+    } else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          });
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
