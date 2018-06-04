a96085948f84fbf9d48941ad159e3fcb493e3286
egoing <egoing@gmail.com>
2018-06-04 09:07:56 +0900

    37.웹앱-글삭제 - 수정버튼 구현

--- main.js
+++ main.js
@@ -55,7 +55,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
             );
             response.writeHead(200);
             response.end(template);
