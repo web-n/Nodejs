35b0b53aa3dcac4c69c113f7b6d087c13284d745
egoing <egoing@gmail.com>
2018-06-04 11:59:41 +0900

    39.JavaScript - 객체 - 형식

--- syntax/object.js
+++ syntax/object.js
@@ -1,5 +1,10 @@
 var members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]); // k8805
+var i = 0;
+while(i < members.length){
+  console.log('array loop', members[i]);
+  i = i + 1;
+}
 
 var roles = {
   'programmer':'egoing',
@@ -7,3 +12,8 @@ var roles = {
   'manager' : 'hoya'
 }
 console.log(roles.designer); //k8805
+console.log(roles['designer']); //k8805
+
+for(var n in roles){
+  console.log('object => ', n, 'value => ', roles[n]);
+}
