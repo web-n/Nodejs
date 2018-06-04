5cf8d94686d92a008352919ce98143d9cec721f5
egoing <egoing@gmail.com>
2018-06-04 11:50:46 +0900

    39.JavaScript - 객체 - 형식

---
+++ syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); // k8805
+
+var roles = {
+  'programmer':'egoing',
+  'designer' : 'k8805',
+  'manager' : 'hoya'
+}
+console.log(roles.designer); //k8805
